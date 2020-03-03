32be911d25f939aa8432bd269198292c967de72c
budzmarooo <budzmarooo@gmail.com>
2020-03-03 14:53:17 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+document.getElementById("prev").addEventListener("click", function() {
+  plusSlides(-1);
+});
+
+document.getElementById("next").addEventListener("click", function() {
+  plusSlides(1);
+});
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+var slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides.length) {
+    slideIndex1 = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1 - 1].style.display = "block";
+  dots[slideIndex1 - 1].className += " active";
+  setTimeout(showSlides1, 3500); // Change image every 2 seconds
+}
+
+window.onscroll = function() {
+  myFunction();
+};
+
+var header = document.getElementById("container");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// menu burger
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+
+const toggleMenu = () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+};
+
+menuBtn.addEventListener("click", toggleMenu);
